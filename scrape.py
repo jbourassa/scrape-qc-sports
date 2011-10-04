d363d012c8a6343c2f8c1469c50c7824d321929c
Antoine Leclair <antoineleclair@gmail.com>
2011-10-04 09:05:27 -0400

    Adds some doctstrings. Adds a _process_tr function for future use.

--- scrape.py
+++ scrape.py
@@ -15,13 +15,15 @@ class Scraper:
         self._url = url
 
     def scrape(self):
+        """Scrapes an url and returns all locations in that html page."""
         html = self._fetch()
         soup = self._soup(html)
         address_tds = soup.findAll("td", attrs={'headers': re.compile('adresse')})
-        addresses = [self._process_td(address) for address in address_tds]
+        addresses = [self._process_address_td(address) for address in address_tds]
         return addresses
 
     def _fetch(self):
+        """Makes the HTTP request and returns the response."""
         conn = httplib.HTTPConnection(self._domain, 80)
         conn.request('GET', self._url)
         response = conn.getresponse()
@@ -29,13 +31,22 @@ class Scraper:
         return html
 
     def _soup(self, html):
+        """Returns a BeauifulSoup object from an html string."""
         return BeautifulSoup(html, convertEntities=BeautifulSoup.HTML_ENTITIES)
 
     def _extract_inner_tags(self, tag):
+        """Removes all the html tags from a BeautifulSoup tag object."""
         html = u''.join([unicode(text) for text in tag.contents])
         return BeautifulSoup(html)
-
-    def _process_td(self, soup_td):
+    
+    def _process_tr(self, soup_tr):
+        """Process a tr and returns a th for the name,
+        td for the address and td for the district."""
+        pass
+        
+    def _process_address_td(self, soup_td):
+        """Processes the td containing the address
+        and returns an address string."""
         soup_td = self._extract_inner_tags(soup_td)
         rosedesvents = soup_td.find('div', attrs={'id': re.compile('rosedesvents')})
         if rosedesvents is not None: rosedesvents.extract()
@@ -46,6 +57,7 @@ class Scraper:
 
 class GeocodingFailureException(Exception): pass
 def geocode_address(address):
+    """Receives an address and return the lat, lng."""
     address = (u'%s, Québec, Qc, Canada' % address).encode('utf-8')
     conn = httplib.HTTPConnection('maps.googleapis.com', 80)
     conn.request('GET', '/maps/api/geocode/json?%s' % urllib.urlencode({
@@ -76,5 +88,3 @@ if __name__ == '__main__':
         json_addresses.append(new_address)
     with open('tennis.json', 'w') as f:
         f.write(json.dumps(json_addresses).encode('utf-8'))
-    
-    #print geocode_address(line)
