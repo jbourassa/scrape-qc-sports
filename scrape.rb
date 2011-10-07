b49fae8a67381e150bce0262f31c83131a843dc3
Marc Lacoursiere <marc@roosoft.com>
2011-10-06 20:37:21 -0400

    Ruby version working for all types of leisure places

--- scrape.rb
+++ scrape.rb
@@ -15,25 +15,41 @@ def getPosition(address)
 end
 
 def cleanAddress(endroit)
-  addressTags = endroit.next.next
+  addressTags = endroit
+  addressTags = addressTags.next while addressTags.name.downcase != 'td'
   addressTags.xpath("span[@class='note']").unlink
-  addressTags.content.gsub(/[\r\n\t]/, ' ').squeeze(' ').strip
+  address = addressTags.content.gsub(/[\r\n\t]/, ' ').squeeze(' ').strip
+  telIndex = address.index('Tél')
+  unless telIndex.nil?
+    address = address[0,telIndex].strip
+  end
+
+  address << ', Quebec, qc'
 end
 
 def getTerrains
-  doc = Nokogiri::HTML(open('http://www.ville.quebec.qc.ca/citoyens/loisirs_sports/tennis.aspx'))
-  endroits = doc.xpath('//th[@headers="endroit"]')
+  loisirs = {
+    :glissoires => 'http://www.ville.quebec.qc.ca/citoyens/loisirs_sports/glissoires.aspx',
+    :parcs => 'http://www.ville.quebec.qc.ca/citoyens/loisirs_sports/parcs_intergenerationnels.aspx',
+    :patinoires => 'http://www.ville.quebec.qc.ca/citoyens/loisirs_sports/patinoires_interieures.aspx',
+    :piscines => 'http://www.ville.quebec.qc.ca/citoyens/loisirs_sports/piscines_interieures.aspx',
+    :raquettes => 'http://www.ville.quebec.qc.ca/citoyens/loisirs_sports/raquettes_ski.aspx'
+  }
+
   terrains = []
-  endroits.each do |endroit| 
-    name = endroit.content.gsub(/[\r\n\t]/, ' ').squeeze(' ').strip 
-    address = cleanAddress(endroit)
-    lat, lng = getPosition address
-    terrains.push({ :name => name, :address => address, :lat => lat, :lng => lng })
+
+  loisirs.keys.each do |loisir|
+    doc = Nokogiri::HTML(open(loisirs[loisir]))
+    endroits = doc.xpath('//th[substring(@headers, 1, 7)="endroit"]')
+    endroits.each do |endroit| 
+      name = endroit.content.gsub(/[\r\n\t]/, ' ').squeeze(' ').strip 
+      address = cleanAddress(endroit)
+      lat, lng = getPosition address
+      terrains.push({ :loisir => loisir, :name => name, :address => address, :lat => lat, :lng => lng })
+    end
   end
+
   terrains
 end
 
-terrains = getTerrains
-terrains.each do |terrain| 
-  puts terrain[:name] << ' lat: ' << terrain[:lat].to_s << ' lng: ' << terrain[:lng].to_s 
-end
+puts getTerrains.to_json
